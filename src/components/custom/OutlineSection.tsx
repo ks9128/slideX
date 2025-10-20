2d644b24d9b56542cb58fad55a553cdf8c7874b0
ks9128 <mdkhalid57470@gmail.com>
2025-10-20 23:15:05 +0530

    feat: Enhance UI components and routing structure

--- src/components/custom/OutlineSection.tsx
+++ src/components/custom/OutlineSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function OutlineSection() {
   return (
     <div className='mt-7'>
-        
+        <h2 className='font-medium text-xl'>Slider Outline</h2>
     </div>
   )
 }
