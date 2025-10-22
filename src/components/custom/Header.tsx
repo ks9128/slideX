dd6868e0640f2e8abcea0255f8e382e5b696a04b
ks9128 <mdkhalid57470@gmail.com>
2025-10-22 08:50:15 +0530

    updated the menu options

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,4 +1,4 @@
-import { SignInButton, UserButton, useUser } from "@clerk/clerk-react";
+import { SignInButton, useAuth, UserButton, useUser } from "@clerk/clerk-react";
 import logo from "../../assets/logo.png";
 import { Button } from "../ui/button";
 import { Link, useLocation } from "react-router";
@@ -6,14 +6,34 @@ import { DiamondIcon, GemIcon } from "lucide-react";
 import { useContext } from "react";
 import { UserDetailContext } from "@/context/UserDetailContext";
 
+const MenuOptions = [
+  {
+    name: 'Workspace',
+    path: '/workspace'
+  },
+  {
+    name: 'Pricing',
+    path: '/workspace/pricing'
+  }
+]
+
 function Header() {
   const { user } = useUser();
   const location = useLocation();
   const { userDetail, setUserDetail } = useContext(UserDetailContext);
   console.log(location.pathname);
+  const { has } = useAuth();
+  const hasUnlimitedAccess = has&&has({ plan: "unlimited" });
   return (
     <div className="flex items-center justify-between px-5 py-1.5 shadow">
       <img src={logo} alt="logo" width={40} height={40} />
+      <ul className="flex gap-10">
+        {MenuOptions.map((menu,index) => (
+          <Link to={menu.path} key={index} className="">
+            <h2>{menu.name}</h2>
+          </Link>
+        ))}
+      </ul>
       {!user ? (
         <SignInButton mode="modal">
           <Button>Get Started</Button>
@@ -22,7 +42,7 @@ function Header() {
         <div className="flex items-center gap-5">
           <UserButton />
           {location.pathname.includes("workspace") ? (
-            <div className="flex items-center p-2 px-3 gap-0.5 bg-orange-100 rounded-full">
+             !hasUnlimitedAccess && <div className="flex items-center p-2 px-3 gap-0.5 bg-orange-100 rounded-full">
               <GemIcon size={'1.2rem'} /> {userDetail?.credits ?? 0}
             </div>
           ) : (
